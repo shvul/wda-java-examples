e66d150efeda10cae092cbec0ffbeaf3ddc6852e
Pavel Serdiukov <iamshvul@gmail.com>
2018-10-27 15:37:55 +0300

    update suite listener

--- src/test/java/com/github/shvul/wda/examples/support/SuiteListener.java
+++ src/test/java/com/github/shvul/wda/examples/support/SuiteListener.java
@@ -4,6 +4,7 @@ import com.github.shvul.wda.client.driver.DriverCapabilities;
 import com.github.shvul.wda.client.driver.DriverFactory;
 import com.github.shvul.wda.client.driver.TVDriver;
 import com.github.shvul.wda.client.support.LoggerManager;
+import org.apache.http.util.Args;
 import org.testng.ISuite;
 import org.testng.ISuiteListener;
 
@@ -17,15 +18,22 @@ public class SuiteListener implements ISuiteListener {
         LoggerManager.enableServerConsoleLogging();
         LoggerManager.enableClientConsoleLogging();
 
-        // setup driver
+        // extract parameters
         Map<String, String> parameters = iSuite.getXmlSuite().getParameters();
+        String deviceId = parameters.get("deviceId");
+        String deviceIp = parameters.get("deviceIp");
+
+        Args.check(!deviceId.isEmpty(),"Specify deviceId in TestNGSuiteConfig.xml");
+        Args.check(!deviceIp.isEmpty(),"Specify deviceIp in TestNGSuiteConfig.xml");
+
+        // setup driver
         DriverCapabilities capabilities = new DriverCapabilities();
         capabilities.setWdaPath("WebDriverAgent/WebDriverAgent.xcodeproj");
         capabilities.setPlatform("tvOS");
         capabilities.setDeviceName("Apple TV");
         capabilities.setBundleId("com.facebook.wda.integrationApp");
-        capabilities.setDeviceIp(parameters.get("deviceIp"));
-        capabilities.setDeviceId(parameters.get("deviceId"));
+        capabilities.setDeviceIp(deviceId);
+        capabilities.setDeviceId(deviceIp);
         capabilities.setAppPath(getClass().getClassLoader().getResource("IntegrationApp_tvOS.ipa").getPath());
         DriverManager.setDriver(DriverFactory.createDriver(capabilities));
     }
