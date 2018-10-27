5aa763d327bf85e2f2d197cc53f729871c6db199
Pavel Serdiukov <iamshvul@gmail.com>
2018-10-27 12:00:31 +0300

    add test groups

---
+++ src/test/java/com/github/shvul/wda/examples/support/Group.java
@@ -0,0 +1,8 @@
+package com.github.shvul.wda.examples.support;
+
+public final class Group {
+    private Group(){}
+
+    public static final String FULL = "Full";
+    public static final String NAVIGATION = "Navigation";
+}
