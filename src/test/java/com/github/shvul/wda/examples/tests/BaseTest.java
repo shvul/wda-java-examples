8688dcaf52e6729078cb83e3efb223e38d47f320
Pavel Serdiukov <iamshvul@gmail.com>
2018-10-27 12:34:44 +0300

    add Navigation Test

---
+++ src/test/java/com/github/shvul/wda/examples/tests/BaseTest.java
@@ -0,0 +1,22 @@
+package com.github.shvul.wda.examples.tests;
+
+import com.github.shvul.wda.client.driver.TVDriver;
+import com.github.shvul.wda.examples.support.DriverManager;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public abstract class BaseTest {
+
+    protected TVDriver driver;
+
+    @BeforeMethod(alwaysRun = true)
+    public void setup() {
+        driver = DriverManager.getDriver();
+        driver.activate();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+        driver.terminate();
+    }
+}
---
+++ src/test/java/com/github/shvul/wda/examples/tests/NavigationTest.java
@@ -0,0 +1,32 @@
+package com.github.shvul.wda.examples.tests;
+
+import com.github.shvul.wda.client.element.TVElement;
+import com.github.shvul.wda.client.element.TVLocator;
+import com.github.shvul.wda.examples.support.Group;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class NavigationTest extends BaseTest {
+
+    @Test(groups = { Group.FULL, Group.NAVIGATION })
+    public void elementFocuseTest() {
+
+        TVElement navigationBtn = driver.findElement(TVLocator.name("Navigation"));
+
+        Assert.assertFalse(navigationBtn.isFocused(),"Navigation button is focused, but shouldn't be.");
+
+        navigationBtn.focuse();
+        Assert.assertTrue(navigationBtn.isFocused(),"Navigation button is not focused.");
+    }
+
+    @Test(groups = { Group.FULL, Group.NAVIGATION })
+    public void elementSelectTest() {
+        TVElement navigationBtn = driver.findElement(TVLocator.name("Navigation"));
+
+        navigationBtn.select();
+        List<TVElement> elements = driver.findElements(TVLocator.predicate("name=\"Templates\""));
+        Assert.assertTrue(elements.size() > 0,"Navigation page is not opened.");
+    }
+}
